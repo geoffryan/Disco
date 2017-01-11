ac4c41227298d854949e6b86f902dfc240ff0b36
Geoff Ryan <geoffrey.s.ryan@gmail.com>
2017-01-11 14:31:15 -0500

    Added GRMHD sorathia setup

--- Initial/sorathia_grmhd.c
+++ Initial/sorathia_grmhd.c
@@ -4,15 +4,21 @@
 static double Mach = 0.0;
 static double gam = 0.0;
 static double M = 0.0;
-static double r0 = 1.0;
-static double r1 = 2.0;
-static double r2 = 4.0;
-static double H0 = 0.2;
+static double r0 = 0.0;
+static double r1 = 0.0;
+static double r2 = 0.0;
+static double H0 = 0.0;
+static int useB = 0;
 
 void setICparams( struct domain * theDomain ){
    Mach = theDomain->theParList.Disk_Mach;
    gam = theDomain->theParList.Adiabatic_Index;
    M = theDomain->theParList.metricPar2;
+   useB = theDomain->theParList.initPar0; //Turn on B field.
+   r0 = theDomain->theParList.initPar1; // Inner edge
+   r1 = theDomain->theParList.initPar2; // Fiducial radius
+   r2 = theDomain->theParList.initPar3; // Outer Edge
+   H0 = theDomain->theParList.initPar4; // Scale Height
 }
 
 double I(double r){
@@ -38,35 +44,39 @@ void initial( double * prim , double * x ){
    double phi = x[1];
    double z   = x[2];
 
-   double rho = 100.0;
+   double rho0 = 100.0;
+   double rho = rho0;
    double omega = sqrt(M/(r*r*r));
    double cs2 = M/(2*r1*r1*r1) * H0*H0; // actually P / rho*h
-   double Pp = (gam-1.0) * cs2 / (gam*(1-cs2)-1) * rho
+   double Pp = (gam-1.0) * cs2 / (gam*(1-cs2)-1) * rho;
 
    double dvp = (2e-2*rnd1 - 1e-2) * omega;
    double vz = (2e-2*rnd2 - 1e-2) * sqrt(M/r0);
    double vr = 0.0;
    double vp = omega +dvp;
 
-   double A0 = 0.37;
-   double Bz = A0*sin(2.*M_PI*(r-r0)/H0)/r*I(r)/sqrt(Rs(r));
+   if(I(r) < 1.0)
+   {
+       vp = omega;
+       vr = 0.0;
+       vz = 0.0;
+   }
 
-   double R2 = r*r+z*z;
-   double R = sqrt(R2);
-   double R3 = R*R2;
+   double A0 = 0.37 * sqrt(M/r0) * sqrt(rho0 / 100.0);
+   if(!useB)
+        A0 = 0.0;
+   double Bz = A0*sin(2.*M_PI*(r-r0)/H0)*I(r)*(r0/r) * sqrt(r0/Rs(r));
 
-   double g00 = -1+2*M/R;
-   double g0r = 2*M*r/R2;
-   double g0z = 2*M*z/R2;
-   double grr = 1+2*M*r*r/R3;
-   double grz = 2*M*r*z/R3;
-   double gzz = 1+2*M*z*z/R3;
+   //Equatorial metric
+   double g00 = -1+2*M/r;
+   double g0r = 2*M/r;
+   double grr = 1+2*M/r;
+   double gzz = 1;
    double gpp = r*r;
-   double u0 = 1.0 / sqrt(-g00 - 2*vr*g0r - 2*vz*g0z - grr*vr*vr - 2*grz*vr*vz
-                            - gzz*vz*vz - gpp*vp*vp);
-   double lr = u0*(g0r+grr*vr+grz*vz); 
+   double u0 = 1.0 / sqrt(-g00 - 2*vr*g0r - grr*vr*vr - gzz*vz*vz - gpp*vp*vp);
+   double lr = u0*(g0r+grr*vr); 
    double lp = u0*gpp*vp; 
-   double lz = u0*(g0z+grz*vr+gzz*vz); 
+   double lz = u0*(gzz*vz); 
 
    prim[RHO] = rho;
    prim[PPP] = Pp;
@@ -78,7 +88,9 @@ void initial( double * prim , double * x ){
    prim[BPP] = 0.0;
    prim[BZZ] = Bz;
 
-   if( NUM_N>0 ) prim[NUM_C] = 0.0;
+   int q;
+   for(q=NUM_C; q<NUM_N; q++)
+       prim[q] = 0.0;
 
 }
 
