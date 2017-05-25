88f92dde5a44e29bfdafda9da7d99d7ae0f13574
Geoff Ryan <geoffrey.s.ryan@gmail.com>
2017-05-24 21:12:05 -0400

    Added some properties to domain

--- Python/discopy/domain.py
+++ Python/discopy/domain.py
@@ -7,6 +7,24 @@ class DiscoDomain:
 
     # A class to contain a Disco Domain
 
+    git = None
+    _pars = None
+    _opts = None
+
+    t = 0.0
+
+    rjph = None
+    zkph = None
+    piph = None
+    
+    prim = None
+    bflux = None
+    planets = None
+    
+    diagRZ = None
+    diagR = None
+    diagZ = None
+
     def __init__(self, checkpointFile=None, parFile=None):
         if checkpointFile is not None:
             self._loadFromCheckpoint(checkpointFile)
@@ -15,8 +33,8 @@ class DiscoDomain:
 
     def __str__(self):
 
-        nr = len(self.rjph)-1
-        nz = len(self.zkph)-1
+        nr = self.numR
+        nz = self.numZ
 
         optkeys = self._opts.keys()
         optkeys.sort()
@@ -25,10 +43,35 @@ class DiscoDomain:
                         key, str(self._opts[key])) for key in optkeys])
         parStr = "NumR: {0:d}, NumZ: {1:d}".format(nr, nz)
 
-        outStr = "DiscoDomain{{t: {0:.8g}, {1:s}, {2:s}}}".format(
+        outStr = "DiscoDomain{{t: {0:.12g}, {1:s}, {2:s}}}".format(
                         self.t, parStr, optStr)
 
         return outStr
+
+    @property
+    def numQ(self):
+        return self.prim[0][0].shape[1]
+
+    @property
+    def numR(self):
+        return self.rjph.shape[0]-1
+
+    @property
+    def numZ(self):
+        return self.zkph.shape[0]-1
+
+    @property
+    def numPhi(self):
+        return np.array([[self.piph[k][j].shape[0] for j in range(self.numR)]
+                            for k in range(self.numZ)])
+
+    @property
+    def R(self):
+        return np.atleast_1d(0.5*(self.rjph[:-1] + self.rjph[1:]))
+
+    @property
+    def Z(self):
+        return np.atleast_1d(0.5*(self.zkph[:-1] + self.zkph[1:]))
 
     def _loadFromCheckpoint(self, filename):
 
@@ -109,3 +152,7 @@ if __name__ == "__main__":
 
     dom = DiscoDomain(sys.argv[1])
     print(dom)
+
+    print(dom.R)
+    print(dom.Z)
+    print(dom.numPhi)
