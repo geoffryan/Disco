cb8699ea09972ff62a1160bb766e4f79431cef82
Geoff Ryan <geoffrey.s.ryan@gmail.com>
2017-07-18 17:53:20 -0400

    udpateParfiles works, cleaned up in.par.template

--- Python/updateParfiles.py
+++ Python/updateParfiles.py
@@ -11,8 +11,8 @@ def loadTemplate(filename):
         words = line.split()
         if len(words) >= 2 and words[0][0].isalpha():
             # Assuming this is a parameter
-            par, rest = words[0], " ".join(words[1:])
-            template.append((par,rest))
+            par = words[0]
+            template.append((par,line))
         else:
             template.append((line,))
 
@@ -30,8 +30,6 @@ def updateParfile(parfile, template):
 
     count0 = len(lines)
 
-    print(lines)
-
     for tline in template:
         if len(tline) == 1:
             if tline[0] not in lines:
@@ -45,22 +43,22 @@ def updateParfile(parfile, template):
                 words = line.split()
                 if len(words) < 2:
                     continue
-                if words[0] is tline[0]:
+                if words[0] == tline[0]:
                     found = True
                     n = i+1
                     break
             if found is False:
-                lines.insert(n, " ".join(tline))
+                lines.insert(n, tline[1])
                 n += 1
 
-    for line in lines:
-        if line[-1] != '\n':
-            line += '\n'
+    for i in range(len(lines)):
+        if lines[i][-1] != '\n':
+            lines[i] += '\n'
 
     count1 = len(lines)
     print("Added {0:d} lines".format(count1-count0))
 
-    f = open(parfile+".par", "w")
+    f = open(parfile, "w")
     f.writelines(lines)
     f.close()
 
